da97aad0443d6001462290fa9dfae272eac1b9f4
JOCHA COLLEGE <gregorykimanthi100@gmail.com>
2024-11-04 17:56:06 +0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./navigation";
+
+export default function App() {
+  return (
+    <div>
+      <Navigation />
+      <h1>hello</h1>
+    </div>
+  );
+}
---
+++ src/components/navigation.jsx
@@ -0,0 +1,15 @@
+import "./navigation.css";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <div className="logo">
+        <h2>devfinder</h2>
+      </div>
+      <div className="theme-switcher">
+        <h2>light</h2>
+        <img src="" alt="light mode icon" />
+      </div>
+    </nav>
+  );
+}
